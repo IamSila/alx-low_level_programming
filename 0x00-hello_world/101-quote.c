8467ec99dd084179c416b5eaccc1a5a4d80b4ff3
IamSila <silamulingi@gmail.com>
2023-09-07 15:15:16 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
  *
- * Return value is  Always 1.
+ * Return: Always 1.
  */
 
 int main(void)
