4e29ffe2e43442c38eb1bd7ee2106b991575cfae
IamSila <silamulingi@gmail.com>
2023-09-07 14:27:40 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 /*
  * programmer was Sila Mulingi
  * 
- * */
+ */
+
+
 #include <stdio.h>
-/*this programme prints a string using 
- * the puts function*/
+
+
+/**
+ * this programme prints a string using 
+ * the puts function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
