34e7a9f4431445a6abafc02dd08fe6f2b8314297
IamSila <silamulingi@gmail.com>
2023-09-07 14:57:54 -0700

    mesaage

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
- * File: 5-printf.c
- * Author is : Sila Mulingi
+ * File-name is  5-printf.c
+ *author is Sila Mulingi
  */
 
 #include <stdio.h>
 
-
 /**
- * main is the  Entry point
- * Return: Always 0
+ * main - Prints the message  "with proper grammar, but the outcome is
+ *                a piece of art," then a new line.
+ *
+ * Return: Always 0.
  */
-
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
