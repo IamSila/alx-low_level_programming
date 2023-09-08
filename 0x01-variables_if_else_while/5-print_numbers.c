b2b48cc39ab7cf215b158b960dbe1cdf29fe1a14
IamSila <silamulingi@gmail.com>
2023-09-08 14:11:52 -0700

    Numbers between 0 and 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+/*authored by Sila Mulingi*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point for the prog
+ *
+ * Return: is always 0
+ *
+ */
+
+int main(void)
+{
+int a;
+a = 0;
+
+while (a <= 9)
+{
+	printf("%d", a);
+	a++;
+}
+putchar('\n');
+
+return (0);
+}
