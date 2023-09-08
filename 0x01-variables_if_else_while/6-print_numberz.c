5541d0292b6ac6f953f7a29a4144c2e53350b13e
IamSila <silamulingi@gmail.com>
2023-09-08 14:20:55 -0700

    print integers using characters

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*written by Sila Mulingi*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- entry point for the programme
+ *
+ * Return: is always zeros(0)
+ *
+ */
+
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
