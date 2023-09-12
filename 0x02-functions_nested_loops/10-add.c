6bf52c276e81ebabb409f2f94f088c2770f9614a
IamSila <silamulingi@gmail.com>
2023-09-12 16:08:31 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adding stuff up mate
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ * author:Sila Mulingi
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
