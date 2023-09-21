0b260290cd68ad050589c3a12640e538b73de8b1
IamSila <silamulingi@gmail.com>
2023-09-21 13:07:49 -0700

    task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _strncpy - is a function that copies n bytes of a source into buffer of a destination string
- *
+ * _strncpy - copies n bytes of a source into buffer of a destination string,
  * @dest: copy source to this buffer
  * @src: this is the source to copy
  * @n: n bytes to be copied
- *
- * Return: the return value is the copied string
+ * Return: copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x;
+	int x; 
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
