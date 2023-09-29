6a208bd38456ebc391f25f003d6a648de1c72b09
IamSila <silamulingi@gmail.com>
2023-09-29 15:35:05 -0700

    static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,22 @@
 #include "main.h"
-#include "2-strlen.c"
+
 /**
- * _puts - puts a string
- *
- * @str: string to print
- * btw nimenstruggle na hii
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
  */
 void _puts(char *str)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	while (count >= 0)
 	{
-		_putchar(*(str + i));
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
