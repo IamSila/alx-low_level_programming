3260dea7bd35b8ba1b175655fd2e3989082ad738
hacker <hacker@kali.hacker>
2024-02-22 17:45:49 +0300

    finding the fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point for the progam
- * @n: hold the limit for the fibonacci numbers
- * Return: value is 0
+ * main - print the first 50 fibonacci numbers
  *
-*/
-
-void generateFibonacci(int n)
-{
-	int a = 1, b = 2, next, i;
-
-	printf("Fibonacci Series of the first %d numbers:\n", n);
-
-	for (i = 0; i < n; ++i)
-{
-	printf("%d", a);
-
-	if (i < n - 1)
-	{
-		printf(", ");
-	}
-
-	next = a + b;
-	a = b;
-	b = next;
-	}
-
-	printf("\n");
-}
+ * Return: Nothing.
+ */
 
 int main(void)
 {
-	int n = 50;
+	int count;
+	unsigned long a, b, next;
 
-	generateFibonacci(n);
+	a = 0;
+	b = 1;
 
+	for (count = 1; count <= 49; count++)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu, ", next);
+	}
+
+	printf("%lu\n", next + a);
 	return (0);
 }
