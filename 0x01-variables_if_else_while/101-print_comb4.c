1855ea170c69939527fe5ebd64a29ec1b3080b1a
hacker <hacker@kali.hacker>
2024-02-27 17:25:20 +0300

    for loops

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Writes combinations of 00-99
- * Description: Writes all unique combinations of 2
- * digit numbers
- * Return: 0 for success
-*/
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int num1, num2;
+	int a, b, l;
 
-for (num1 = 0; num1 <= 99; num1++)
-{
-for (num2 = 0; num2 <= 99; num2++)
-{
-if (num1 < num2)
-{
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
-putchar(' ');
-putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');
-
-if (num1 != 98 || num2 != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(l);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
