c2eff1841f1e656cd4385d86b5d28071cb1e3383
IamSila <silamulingi@gmail.com>
2024-07-22 13:01:22 +0300

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: separates the numbers
+ * @n: the index of the number
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ag;
+	unsigned int index;
+
+	va_start(ag, n);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(ag, int));
+		if (index != n - 1)
+		{
+			printf("%s", separator);
+		}
+		if (index == n - 1)
+		{
+			printf("\n");
+		}
+	}
+	va_end(ag);
+}
