db3f49038ec1050fd4cb057647c9155e391ac0cc
IamSila <silamulingi@gmail.com>
2025-02-15 13:13:16 +0300

    Printing alphabets from a to z and A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print small and uppercase alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char small;
+	char capital;
+
+	small = 'a';
+	capital = 'A';
+
+	while (small < 'z')
+	{
+		putchar(small);
+		small++;
+	}
+	while (capital < 'Z')
+	{
+		putchar(capital);
+		capital++;
+	}
+	putchar('\n');
+	return (0);
+}
+
