579ce4bd49e78daa70832a9b49aa4ad6d2118907
IamSila <silamulingi@gmail.com>
2025-02-23 01:26:23 +0300

    fxn to print all alpabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lowercase
+ * Return: nothing
+ * author - Sila Mulingi [https://www.github.com/IamSila]
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while(letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
