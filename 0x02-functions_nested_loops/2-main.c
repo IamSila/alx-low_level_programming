6c597e18cf869b0b879334cfc66b98c8dcbad4a5
IamSila <silamulingi@gmail.com>
2025-02-23 01:40:44 +0300

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: is always 0 on success
+ * author - Sila mulingi [https://www.github.com/IamSila]
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
+
