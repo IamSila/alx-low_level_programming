f0bb3eb73e4e7d031e2c453a59a03d34ac886832
IamSila <silamulingi@gmail.com>
2025-02-23 03:27:50 +0300

    Sign

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * Return: is always 0
+ *
+ * author - Sila Mulingi [https://www.github.com/IamSila]
+ */
+
+int main(void)
+{
+	int r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_sign - prints the sign of a integer value
+ * @n: the integer whose sign in is cheked
+ *
+ * author: Sila Mulingi [https://www.github.com/IamSila]
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	
+}
+
