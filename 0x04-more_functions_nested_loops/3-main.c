13e034675297c26e36b4646f62b049365c901b2f
IamSila <silamulingi@gmail.com>
2025-03-19 03:22:21 +0300

    Printing numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the main code
+ * Return: is always 0
+ * author - Sila Mulingi [https://www.github.com/IamSila]
+ */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
+
