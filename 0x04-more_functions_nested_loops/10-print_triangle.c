e26865c6dc338dccadc35c9310ec2dbf94d03593
IamSila <silamulingi@gmail.com>
2025-03-20 12:45:00 +0300

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_triangle - prints a triangle on the console
- * @size: the height and length of the triangle
- * Return: Nothing
- * author - Sila Mulingi [https://www.github.com/IamSila]
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
-
 void print_triangle(int size)
 {
-	int i;
-	int j;
-	int k;
-
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= size; j++)
-		{
-			_putchar(' ');
-			if (j >= k)
-			{
-				for (k = size; k >= 1; k--)
-				{
-					_putchar('#');
-				}
-			}
-		}
 		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
