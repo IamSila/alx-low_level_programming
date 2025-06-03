54bc4a4f2554d98a98bcd56bd0a1bf7013917bc9
IamSila <silamulingi@gmail.com>
2025-06-03 11:32:57 +0300

    0-reset to 98 function done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - Overwrites a variable's value to 98
+ * @n: pointer to the variable's memory location
+ * Return: The new intgeger value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
