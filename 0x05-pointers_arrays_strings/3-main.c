89d07a3bec3dd4fdc31d1e923d32be31cc4a3c88
IamSila <silamulingi@gmail.com>
2025-06-07 02:41:09 +0300

    printing values with the putchar fxn

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: Always 0 for success, non-zero for failure
+ */
+
+int main(void)
+{
+	char *str;
+	
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
+
