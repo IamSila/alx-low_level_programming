89e6ca78685cb1d73da23c0453c5691ed6ea031b
IamSila <silamulingi@gmail.com>
2025-06-06 14:40:06 +0300

    counting the characters of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,18 @@
 #include "main.h"
+#include <linux/limits.h>
+
 /**
  * _strlen - function to return the length of a string
  * @s: string to return length for
  * Return: the length of a string
  */
 
-int _strlen(char *s) {
-  int i;
+int _strlen(char *s)
+{
+	int i;
 
-  for (i = 0; s[i] != '\0'; i++) {
-  }
-  return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
