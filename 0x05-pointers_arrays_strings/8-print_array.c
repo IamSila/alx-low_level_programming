d12239298abb1757d732dc8079e4ce125146d82d
IamSila <silamulingi@gmail.com>
2025-06-07 15:22:06 +0300

    printing x number of elements from an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "2-strlen.c"
+#include <stdio.h>
+
+
+/**
+ * print_array - prints x elements of an array
+ * @a: the array to store our elements.
+ * @n: the number of arrays we need.
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d",(*(a + i)));
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
