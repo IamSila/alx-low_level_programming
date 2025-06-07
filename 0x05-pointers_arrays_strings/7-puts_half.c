94ff6f582d80fd2402acc8d353e9d488c799ffc0
IamSila <silamulingi@gmail.com>
2025-06-07 14:39:09 +0300

    printing second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,24 @@
 #include "main.h"
 #include "2-strlen.c"
-#include <stdio.h>
 
 /**
- * puts_half - Prints one half of a string
- * @str: the string we work with
- * Return: Nothing
+ * puts_half - puts half of int
+ *
+ * @str: string to split
  */
-
 void puts_half(char *str)
 {
-	int length;
-	int half_length;
 	int i;
+	int x = 0;
 
-
-	length = _strlen(str);
-	if (length % 2 == 0)
+	if (_strlen(str) % 2 != 0)
 	{
-		half_length = length / 2;
+		x += 1;
 	}
-	else
-	{
-		half_length = (length - 1) / 2;
-	}
-	for (i = half_length; i < length; i++)
+	for (i = (_strlen(str) + x) / 2; i < _strlen(str); i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
+
