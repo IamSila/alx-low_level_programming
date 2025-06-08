46fccfc937cfdb0898b8a1e3cf1ef90ff5b713c7
IamSila <silamulingi@gmail.com>
2025-06-08 12:38:35 +0300

    copying a string into another string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "2-strlen.c"
+#include <stdio.h>
+
+
+/**
+ * _strcpy - copies one string into another
+ * @dest: final string
+ * @src - source string to copy from
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+	int i;
+	
+
+	length = _strlen(src);
+	for (i = 0; i < length || i == '\n'; i++)
+	{
+		dest[i] = src[i];
+	}
+	printf("\n");
+	return (dest);
+}
+
