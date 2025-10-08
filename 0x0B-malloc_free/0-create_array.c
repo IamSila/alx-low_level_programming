2d7d93772a00049622b16afb932ed21002064082
IamSila <silamulingi@gmail.com>
2025-10-08 07:14:53 +0300

    memory management with malloc is fun

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,29 +3,30 @@
 #include "main.h"
 
 /**
- * crate_array - creates an array of characters
+ * *create_array - creates an array of
+ * characters and initializes with character c
  * @size: the number of characters to create
  * @c: the character to fill the array
- * Return - the string created
+ * Return: pointer to the array (Success), NULL (Error)
  */
 
 char *create_array(unsigned int size, char c)
 {
-    char *p;
-    unsigned int i = 0;
+	char *p;
+	unsigned int i = 0;
 
-    if (size == 0)
-        return (NULL);
-    p = malloc(size * sizeof(c));
-    if (p == NULL)
-    {
-        return (0);
-    }
-    while (i < size)
-    {
-        *(p + i) = c;
-        i++;
-    }
-    *(p + i) = '\0';
-    return (p);
+	if (size == 0)
+		return (NULL);
+	p = malloc(size * sizeof(c));
+	if (p == NULL)
+	{
+		return (0);
+	}
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+	*(p + i) = '\0';
+	return (p);
 }
