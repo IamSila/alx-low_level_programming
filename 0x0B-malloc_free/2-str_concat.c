e235596cd8547e34f1cb0203372962df872490af
IamSila <silamulingi@gmail.com>
2025-10-15 04:39:01 +0300

    great challenge: concating s1 and s2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * str_concat - concat s1 and s2 in that order
@@ -10,20 +11,20 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int len_s1 = strlen(s1);
+	int len_s2 = strlen(s2);
+	int length = len_s1 + len_s2;
+	char *s3 = malloc((length + 1) * sizeof(char));
+	char *temp = s3;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	int len_s1 = strlen(s1);
-	int len_s2 = strlen(s2);
-	int length = len_s1 + len_s2;
-	char *s3 = malloc((length + 1) * sizeof(char));
-
 	if (s3 == NULL)
 		return (NULL);
 	/** this acts as the moving cursor; */
-	char *temp = s3;
 
 	while (*s1)
 		*temp++ = *s1++;
@@ -32,5 +33,5 @@ char *str_concat(char *s1, char *s2)
 		*temp++ = *s2++;
 
 	*temp = '\0';
-		return (s3);
+	return (s3);
 }
