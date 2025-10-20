e4f3a7cb39d4910765d7ea2f4575d1964093fb62
IamSila <silamulingi@gmail.com>
2025-10-20 09:54:36 +0300

    betty checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocated memory to a variable
+ * *malloc_checked - allocated memory to a variable
  * @b: the size of memory
- * Return - a pointer to the allocated memory
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
+
 	if (mem == NULL)
 	{
 		exit(98);
