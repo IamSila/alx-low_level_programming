35f20120d0f92e53aa7b73a6dd80e5260c0333c0
IamSila <silamulingi@gmail.com>
2025-10-20 09:35:59 +0300

    mem check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocated memory to a variable
+ * @b: the size of memory
+ * Return - a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
