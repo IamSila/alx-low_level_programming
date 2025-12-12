30a0d9c24f1406e837774479fefcdb8aeec0e280
IamSila <silamulingi@gmail.com>
2025-12-12 19:55:29 +0300

    init_dog creates a new element of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 
 
 
@@ -12,6 +12,10 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("struct is not provided");
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
