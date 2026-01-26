dcac5216b897d7d7ca55c1f9c629a88a45bf5bba
IamSila <silamulingi@gmail.com>
2026-01-27 00:51:29 +0300

    sum function: printing string paramerters of a vadiac function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,43 +1,33 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_strings - print strings provided as parameters.
- * @separator: string printed between the strings.
- * @n: number of strings provided to the function.
- * Return: Nothing.
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
 
-void print_strings(const char *separator, const unsigned int n, ...){
-    va_list args;
-    unsigned int x;
-    char *str;
+	va_list list;
 
-    va_start(args, n);
+	va_start(list, n);
 
-    for (x = 0; x < n; x++)
-    {
-        str = va_arg(args, char *);
-        if (!str)
-        {
-            str = "nil";
-        }
-        if (!separator)
-        {
-            printf("%s", str);
-        }
-        else
-        {
-            printf("%s", str);
-            if (x < n - 1)
-            {
-                printf("%s", separator);
-            }
-        }
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
 
-    }
-    va_end(args);
-    printf("\n");
+	printf("\n");
+
+	va_end(list);
 }
-
