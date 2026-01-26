adfbb8d517f8595c91bb1527b2ddc04d938d5f1b
IamSila <silamulingi@gmail.com>
2026-01-26 23:06:21 +0300

    sum function: corrected function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -28,24 +28,3 @@ int sum_them_all(const unsigned int n, ...)
 	return (results);
 }
 
-
-
-
-
-
-
-/**
- * main - the entry point of the program
- * Return: Always 0 for success.
- */
-
-int main(void)
-{
-	int sum;
-
-	sum = sum_them_all(2, 98, 1024);
-	printf("%d\n", sum);
-	sum = sum_them_all(4, 98, 1024, 402, -1024);
-	printf("%d", sum);
-	return (0);
-}
