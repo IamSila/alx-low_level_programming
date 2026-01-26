c27d21ef341d3032a8441cd22d32400c390bc68b
IamSila <silamulingi@gmail.com>
2026-01-26 23:36:18 +0300

    sum function: printing values of a vadiac function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,11 +25,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     for (x = 0; x < n; x++)
     {
         printf("%d", va_arg(args, int));
-        if (x <= n - 2)
+        if (x != n - 1)
         {
             printf("%s", separator);
         }
     }
-    printf("\n");
     va_end(args);
+    printf("\n");
 }
